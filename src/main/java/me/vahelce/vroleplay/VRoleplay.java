30413b92f9e3cd4f10125f19fa6e525591b8db7a
vahelce <nabatnikov.dmitrii@gmail.com>
2020-07-14 20:08:27 +0200

    Add help command

--- src/main/java/me/vahelce/vroleplay/VRoleplay.java
+++ src/main/java/me/vahelce/vroleplay/VRoleplay.java
@@ -1,13 +1,19 @@
 package me.vahelce.vroleplay;
 
 import me.vahelce.vroleplay.commands.*;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.List;
 
-public class VRoleplay extends JavaPlugin {
+public class VRoleplay extends JavaPlugin implements CommandExecutor {
     private static VRoleplay instance;
     private static Configuration configuration;
+    private static String[] helpMessages;
 
     @Override
     public void onLoad() {
@@ -20,6 +26,11 @@ public class VRoleplay extends JavaPlugin {
         if (!new File(filePath).exists())
             saveResource("config.yml", true);
         configuration = new Configuration(filePath, this);
+        List<String> rawHelp = (List<String>) configuration.getRawValue("message.help");
+        helpMessages = new String[rawHelp.size()];
+        for (int i = 0; i < rawHelp.size(); i++) {
+            helpMessages[i] = ChatColor.translateAlternateColorCodes('&', rawHelp.get(i));
+        }
         registerCommands();
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
     }
@@ -30,6 +41,18 @@ public class VRoleplay extends JavaPlugin {
         getServer().getPluginCommand("do").setExecutor(new DoCommand());
         getServer().getPluginCommand("b").setExecutor(new OocChatCommand());
         getServer().getPluginCommand("mask").setExecutor(new MaskCommand());
+        getServer().getPluginCommand("vroleplay").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage("§6V§eRolePlay §rv. " + getDescription().getVersion());
+            sender.sendMessage("§eEnter /vroleplay §fhelp §7for help.");
+        } else {
+            sender.sendMessage(helpMessages);
+        }
+        return true;
     }
 
     public static VRoleplay getInstance() {
